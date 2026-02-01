8ba0092093caef3b807c804ea0be483d2348f944
SohamWadhwa <soham.wadhwa2006@gmail.com>
2026-02-01 18:33:40 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,9 +5,9 @@ import { Analytics } from '@vercel/analytics/next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'Portfolio - Kartik Wadhwa',
-  description: 'Created with v0',
-  generator: 'v0.app',
+  title: 'Portfolio - Aman Gupta',
+  description: '',
+  generator: '',
   icons: {
     icon: [
       { url: '/favicon.ico' },
